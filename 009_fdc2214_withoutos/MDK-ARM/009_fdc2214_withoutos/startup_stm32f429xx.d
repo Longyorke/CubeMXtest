e2fe5f439408d93d6b9e44fc38c3991ea93d5a13
LongYorke <1269217124@qq.com>
2021-07-22 19:34:10 +0800

    Add FDC2214 project template

---
+++ 009_fdc2214_withoutos/MDK-ARM/009_fdc2214_withoutos/startup_stm32f429xx.d
@@ -0,0 +1,1 @@
+009_fdc2214_withoutos\startup_stm32f429xx.o: startup_stm32f429xx.s
---
+++ 009_fdc2214_withoutos/MDK-ARM/RTE/_009_fdc2214_withoutos/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '009_fdc2214_withoutos' 
+ * Target:  '009_fdc2214_withoutos' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
