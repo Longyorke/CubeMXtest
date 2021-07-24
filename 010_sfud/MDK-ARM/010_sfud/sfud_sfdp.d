a39d49f928ae3408790622ab3f9c5c4d7435e13c
LongYorke <1269217124@qq.com>
2021-07-24 19:24:28 +0800

    011：Test success

--- 010_sfud/MDK-ARM/010_sfud/sfud_sfdp.d
+++
@@ -1,9 +0,0 @@
-010_sfud\sfud_sfdp.o: ..\Package\sfud\src\sfud_sfdp.c
-010_sfud\sfud_sfdp.o: ..\Package\sfud\inc\sfud.h
-010_sfud\sfud_sfdp.o: ..\Package\sfud\inc\sfud_def.h
-010_sfud\sfud_sfdp.o: D:\software\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-010_sfud\sfud_sfdp.o: D:\software\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-010_sfud\sfud_sfdp.o: D:\software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-010_sfud\sfud_sfdp.o: D:\software\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-010_sfud\sfud_sfdp.o: ..\Package\sfud\inc\sfud_cfg.h
-010_sfud\sfud_sfdp.o: ..\Package\sfud\inc\sfud_flash_def.h
--- 010_sfud/MDK-ARM/010_sfud/startup_stm32f429xx.d
+++
@@ -1,1 +0,0 @@
-010_sfud\startup_stm32f429xx.o: startup_stm32f429xx.s
